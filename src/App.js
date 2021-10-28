652c634b504dcc49a40445fb2c3e8f653a4c4748
Benny Li <96bennyl@gmail.com>
2021-10-27 18:36:40 -0700

    add insert, modify, delete form to ingredients. fix column names

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Container from '@mui/material/Container';
 import Navigation from './components/Navigation';
 import Tables from './components/Tables'
 import RecipeInput from './components/RecipeInput'
+import { Button } from '@mui/material';
+import { Grid } from '@mui/material';
+import { TextField } from '@mui/material';
 
 function App() {
   // DATA (will be retrieved via DB)
   // RECIPES
   const recipe_columns = [
-    { field: 'id', headerName: 'ID', width: 75 },
-    { field: 'recipeTitle', headerName: 'recipeName', width: 200 },
+    { field: 'id', headerName: 'recipeID', width: 150 },
+    { field: 'recipeTitle', headerName: 'recipeTitle', width: 200 },
     { field: 'recipeServing', headerName: 'recipeServing', type: 'number', width: 150 },
     { field: 'recipeDescription', headerName: 'recipeDescription', width: 800 },
   ];
@@ -33,7 +36,7 @@ function App() {
 
   // INGREDIENTS
   const ingr_columns = [
-    { field: 'id', headerName: 'ID', width: 75 },
+    { field: 'id', headerName: 'ingredientID', width: 150 },
     { field: 'ingredientName', headerName: 'ingredientName', width: 200 },
     { field: 'price', headerName: 'price', type: 'number', width: 90 }
   ];
@@ -47,87 +50,67 @@ function App() {
 
   // USERS
   const user_columns = [
-    { field: 'id', headerName: 'ID', width: 75 },
-    { field: 'firstName', headerName: 'First name', width: 150 },
-    { field: 'lastName', headerName: 'Last name', width: 150 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''
-        }`,
-    },
+    { field: 'id', headerName: 'customerID', width: 150 },
+    { field: 'cartID', headerName: 'cartID', width: 150 },
+    { field: 'fName', headerName: 'fName', width: 150 },
+    { field: 'lName', headerName: 'lName', width: 150 },
+    { field: 'email', headerName: 'email', width: 150 },
+    { field: 'zipCode', headerName: 'zipCode', width: 150 },
   ]
 
   const user_rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, cartID: 1, lName: 'Snow', fName: 'Jon', email:'example@ex.com', zipCode:'00000' },
+    { id: 2, cartID: 2, lName: 'Targ', fName: 'Dany', email:'example@ex.com', zipCode:'00000' },
+    { id: 3, cartID: 3, lName: 'Snow', fName: 'Rob', email:'example@ex.com', zipCode:'00000' },
+    { id: 4, cartID: 4, lName: 'Lannister', fName: 'Cersei', email:'example@ex.com', zipCode:'00000' },
+
   ]
 
   // SHOPPING CARTS
   const cart_columns = [
-    { field: 'id', headerName: 'ID', width: 75 },
-    { field: 'cartOwner', headerName: 'cartOwner', width: 100 },
+    { field: 'id', headerName: 'cartID', width: 150 },
+    { field: 'customerID', headerName: 'customerID', width: 150 },
     // add a function to get the full name of the owner from the user data
   ]
 
   const cart_rows = [
-    { id: 1, cartOwner: 1 },
-    { id: 2, cartOwner: 3 },
-    { id: 3, cartOwner: 6 },
-    { id: 4, cartOwner: 9 }
+    { id: 1, customerID: 1 },
+    { id: 2, customerID: 2 },
+    { id: 3, customerID: 3 },
+    { id: 4, customerID: 4 }
   ]
 
   // SELECTED RECIPES
   const selected_recipe_columns = [
-    { field: 'id', headerName: 'ID', width: 75 },
     // should be pulled from shopping carts
-    { field: 'cartID', headerName: 'cartID', width: 75 },
+    { field: 'id', headerName: 'cartID', width: 75 },
     { field: 'recipeID', headername: 'recipeID', width: 100 },
     // should be pulled from recipes
-    { field: 'recipeTitle', headername: 'recipeName', width: 200 }
+    { field: 'selectedQuantity', headername: 'selectedQuantity', width: 200 }
   ]
 
   const selected_recipe_rows = [
-    { id: 1, cartID: 1, recipeID: 1, recipeTitle: "Chicken Breast" },
-    { id: 2, cartID: 1, recipeID: 2, recipeTitle: "Chicken Noodle Soup" },
-    { id: 3, cartID: 1, recipeID: 4, recipeTitle: "Chicken Dumpling" },
+    { id: 1, recipeID: 1, selectedQuantity: 1 },
+    { id: 2, recipeID: 2, selectedQuantity: 3 },
+    { id: 3, recipeID: 4, selectedQuantity: 2 },
+    { id: 3, recipeID: 2, selectedQuantity: 2 },
   ]
 
   // RECIPE INGREDIENTS
   const recipe_ingr_columns = [
-    { field: 'id', headerName: "ID", width: 75 },
-    { field: 'recipeID', headerName: 'recipeID', width: 125 },
-    // pull from recipes
-    { field: 'recipeName', headername: 'recipeName', width: 200 },
+    { field: 'id', headerName: 'recipeID', width: 125 },
     { field: 'ingredientID', headername: "ingredientID", width: 125 },
+    // pull from recipes
     // pull from ingredients
-    { field: 'ingredientName', headername: 'ingredientName', width: 200 },
+    { field: 'ingredientQuantity', headername: 'ingredientQuantity', width: 200 },
     { field: 'uom', headerName: "unit of measurement", width: 200 },
-
   ]
 
   const recipe_ingr_rows = [
-    { id: 1, recipeID: 1, recipeName: "Chicken Breast", ingredientID: 1, ingredientName: "Chicken Breast", uom: "1 white meat chicken breast" },
-    { id: 2, recipeID: 1, recipeName: 'Chicken Breast', ingredientID: 200, ingredientName: 'Sea Salt', uom: '2 tsp' },
-    { id: 3, recipeID: 1, recipeName: 'Chicken Breast', ingredientID: 201, ingredientName: 'Black Pepper', uom: '1 tsp' },
-    { id: 4, recipeID: 1, recipeName: 'Chicken Breast', ingredientID: 4200, ingredientName: 'Olive Oil', uom: '2 tbsp' },
+    { id: 1, ingredientID: 1, ingredientQuantity: 5, uom: "g" },
+    { id: 2, ingredientID: 1, ingredientQuantity: 6, uom: 'tsp' },
+    { id: 3, ingredientID: 3, ingredientQuantity: 6, uom: 'tsp' },
+    { id: 4, ingredientID: 4, ingredientQuantity: 2, uom: 'tbsp' },
   ]
 
   return (
@@ -141,9 +124,67 @@ function App() {
           </Route>
           <Route path='/ingredients'>
             <Tables columns={ingr_columns} rows={ingr_rows} />
+            <div>
+              <Grid container spacing={2} sx={{ width: 95 / 100, marginLeft: 'auto', marginRight: 'auto' }}>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='Ingredient Name' variant='outlined'/>
+                  </Grid>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='price' variant='outlined'/>
+                  </Grid>
+                  <Grid item sx={{ my: 'auto' }}>
+                      <Button variant="outlined"> Add </Button>
+                  </Grid>
+                </Grid>
+            </div>
+            <div style={{marginTop:10}}>
+              <Grid container spacing={2} sx={{ width: 95 / 100, marginLeft: 'auto', marginRight: 'auto' }}>
+              <Grid item>
+                      <TextField id='outlined-basic' label='Ingredient ID' variant='outlined'/>
+                  </Grid>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='Ingredient Name' variant='outlined'/>
+                  </Grid>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='price' variant='outlined'/>
+                  </Grid>
+                  <Grid item sx={{ my: 'auto' }}>
+                      <Button variant="outlined"> Modify </Button>
+                  </Grid>
+                </Grid>
+            </div>
+            <div style={{marginTop:10}}>
+              <Grid container spacing={2} sx={{ width: 95 / 100, marginLeft: 'auto', marginRight: 'auto' }}>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='Ingredient ID' variant='outlined'/>
+                  </Grid>
+                  <Grid item sx={{ my: 'auto' }}>
+                      <Button variant="outlined"> Delete </Button>
+                  </Grid>
+                </Grid>
+            </div>
           </Route>
           <Route path='/users'>
             <Tables columns={user_columns} rows={user_rows} />
+            <div>
+              <Grid container spacing={2} sx={{ width: 95 / 100, marginLeft: 'auto', marginRight: 'auto' }}>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='fName' variant='outlined'/>
+                  </Grid>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='lName' variant='outlined'/>
+                  </Grid>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='email' variant='outlined'/>
+                  </Grid>
+                  <Grid item>
+                      <TextField id='outlined-basic' label='zipCode' variant='outlined'/>
+                  </Grid>
+                  <Grid item sx={{ my: 'auto' }}>
+                      <Button variant="outlined"> Add </Button>
+                  </Grid>
+                </Grid>
+            </div>
           </Route>
           <Route path='/shoppingcarts'>
             <Tables columns={cart_columns} rows={cart_rows} />
