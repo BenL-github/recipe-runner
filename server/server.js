2bad345fdd9db7af3258772868dc8a20ca4c4ba5
Timothy Jan <tj.jan@outlook.com>
2021-10-27 23:22:21 -0700

    added express backend
    script in src/package.json to create production build and start server;
    run npm start from within src

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const express = require('express');
+const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "..", "build")))
+app.use(express.static("public"));
+const port = process.env.PORT || 9000;
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  });
+
+// app.get("/", (req, res) => {
+//     res.send("This is from express.js");
+//   });
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
