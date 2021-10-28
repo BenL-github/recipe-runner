d7ac1adffc657211641dd6a1f76b56beefa76e78
Timothy Jan <tj.jan@outlook.com>
2021-10-28 00:16:38 -0700

    updated port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "..", "build")))
 app.use(express.static("public"));
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 34873;
 
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "..", "build", "index.html"));
