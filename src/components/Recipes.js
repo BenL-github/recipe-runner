6c740ef09e279102a9d6593a45f385592b8eaa14
Timothy Jan <tj.jan@outlook.com>
2021-11-03 19:04:15 -0700

    implemented search for recipes, saved startup scripts

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,16 +2,15 @@ import { useState } from 'react';
 import Tables from './Tables'
 import RecipeInput from './RecipeInput'
 import { Typography } from '@mui/material';
+import { Grid } from '@mui/material';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
 
 function Recipes() {
-    // RECIPES
-    // behavior when user adds a new recipe
-    const onNewRecipe = ({ title, serving, description }) => {
-        // will be a PUT to the database
-        const new_recipe = { id: Math.floor(Math.random() * 10000), recipeTitle: title, recipeServing: serving, recipeDescription: description }
-        set_recipe_rows([...recipe_rows, new_recipe])
-
-    }
+    const [keyword, setKeyword] = useState("")
+    const [isResults, setIsResults] = useState(false)
+    const [isNoResults, setIsNoResults] = useState(false)
+    const [searchRows, setSearchRows] = useState([])
 
     const recipe_columns = [
         { field: 'id', headerName: 'recipeID', width: 150 },
@@ -28,11 +27,68 @@ function Recipes() {
         { id: 4, recipeTitle: 'Chicken Dumpling', recipeServing: 2, recipeDescription: 'mmm it taste good' },
     ]);
 
+    // behavior when user adds a new recipe
+    const onNewRecipe = ({ title, serving, description }) => {
+        // will be a PUT to the database
+        const new_recipe = { id: Math.floor(Math.random() * 10000), recipeTitle: title, recipeServing: serving, recipeDescription: description }
+        set_recipe_rows([...recipe_rows, new_recipe])
+
+    }
+
+    // behavior when user searches for a recipe
+    const onSearch = () => {
+        // query database for matching SELECT
+        // const someFuncHere
+        let rows = []
+        console.log(keyword)
+        recipe_rows.map((row) => {
+            let title = row.recipeTitle.toLowerCase()
+            if (title.includes(keyword)) {
+                rows.push(row)
+                setIsResults(true)
+                setIsNoResults(false)
+            }
+        })
+
+        setSearchRows(rows)
+
+        if (rows.length < 1) {
+            setIsNoResults(true)
+            setIsResults(false)
+        }
+    }
+
     return (
         <>
             <Typography variant='h2'>Recipes</Typography>
             <Tables columns={recipe_columns} rows={recipe_rows} />
+            <Typography variant='h3'>Enter New Recipe</Typography>
             <RecipeInput buttonText={'Create New Recipe'} onNewRecipe={onNewRecipe} />
+            <Typography variant='h3'>Search for a Recipe</Typography>
+
+            {/* Search Field */}
+            <Grid container spacing={2} sx={{ width: 95 / 100, marginLeft: 'auto', marginRight: 'auto' }}>
+                <Grid item>
+                    <TextField id='outlined-basic' label='Title Keyword' variant='outlined'
+                        onChange={(e) => setKeyword(e.target.value)} />
+                </Grid>
+                <Grid item sx={{ my: 'auto' }}>
+                    <Button variant="outlined" onClick={onSearch}> Search </Button>
+                </Grid>
+            </Grid>
+
+            {/* No Matching Results */}
+            {isNoResults && <>
+                <Typography variant='h4'>Search Results</Typography>
+                <Typography variant='p'>{`No recipes with the keyword "${keyword}"; try again with a different keyword or add a new recipe!`}</Typography>
+            </>}
+
+            {/* Results Found! */}
+            {isResults && <>
+                <Typography variant="h4">Search Results</Typography>
+                <Tables columns={recipe_columns} rows={searchRows} />
+            </>}
+
         </>
     )
 }
