dc7a819c0dd71a40bf86cbbaa23d409aa8ea8922
Timothy Jan <tj.jan@outlook.com>
2021-11-05 13:54:11 -0700

    queries and setup

---
+++ recipe_runner_queries.sql
@@ -0,0 +1,84 @@
+-- FOR ALL QUERIES: a leading * denotes variables that will have data from node.js
+
+-- RECIPES  
+-- 1. Get all recipes to display in page's main table
+SELECT * FROM `Recipes`;
+
+-- 2. Add a new recipe
+INSERT INTO `Recipes` (recipeTitle, recipeDescription, recipeServing)
+VALUES (*recipeTitleInput, *recipeDescriptionInput, *recipeServingInput);
+
+-- 3. Search for a recipe
+SELECT * FROM `Recipes` WHERE * `recipeTitle` LIKE %*recipeTitleInput%;
+
+-- 4. Delete a recipe
+DELETE FROM `Recipes` WHERE `recipeID`=*recipeIDInput;
+
+
+-- INGREDIENTS
+-- 1. Get all ingredients for display in page's main table
+SELECT * FROM `Ingredients`;
+
+-- 2. Add a new ingredient
+INSERT INTO `Ingredients` (ingredientName, price)
+VALUES (*ingredientNameInput, *ingredientPriceInput);
+
+-- 3. Update an existing ingredient
+UPDATE `Ingredients` 
+SET ingredientName=*ingredientNameInput, price=*ingredientPriceInput
+WHERE ingredientID=*ingredientIDInput;
+
+
+-- USERS
+-- 1. Get all users for display in page's main table
+SELECT * FROM `Users`;
+
+-- 2. Add a new user
+INSERT INTO `Users` (fName, lName, email, zipCode)
+VALUES (*fNameInput, *lNameInput, *emailInput, *zipCodeInput);
+
+-- 3. Update a user
+UPDATE `Users`
+SET fName=*fNameInput, lName=*lNameInput, email=*emailInput, zipCode=*zipCodeInput
+WHERE customerID=*customerIDInput;
+
+
+-- SHOPPING CARTS
+-- 1. Get all shopping carts for display in page's main table with names from Users
+SELECT cartID, cartOwner, fullName AS (fName, lName) FROM `ShoppingCarts`
+JOIN `Users` ON `Users`.customerID = `ShoppingCarts`.cartOwner
+GROUP BY cartID; 
+
+-- 2. Add a new Shopping Cart
+-- There will be validation in the program ensure customerID exists and doesn't already have a cart
+INSERT INTO `ShoppingCarts` (cartID, customerID)
+VALUES (*cartIDInput, *userIDInput);
+
+
+-- SELECTED RECIPES
+-- 1. Get all selected recipes for display in page's main table
+-- includes name of recipes and name of cart owner
+SELECT cartID, fullName AS (fName, lName), recipeID, recipeTitle, selectedQuantity FROM `SelectedRecipes`
+JOIN `ShoppingCarts` ON `ShoppingCarts`.cartID = `SelectedRecipes`.cartID
+JOIN `Users` ON `Users`.customerID = `ShoppingCarts`.cartOwner
+JOIN `Recipes` ON `Recipes`.recipeID = `SelectedRecipes`.recipeID
+GROUP BY cartID;
+
+-- 2. Add a new selected recipe (add a recipe to the cart)
+-- backend validation to ensure a cartID and recipeID are valid
+INSERT INTO `SelectedRecipes` (selectedCart, selectedRecipe, selectedQuantity)
+VALUES (*selectedCartIDInput, *selectedRecipeIDInput, *selectedQuantityInput);
+
+
+-- RECIPE INGREDIENTS
+-- 1. Get all recipe ingredients for display in page's main table
+-- includes name of recipe and name of ingredient
+SELECT recipeID, recipeTitle, ingredientID, ingredientName, ingredientQuantity, uOm FROM `RecipeIngredients`
+JOIN `Ingredients` ON `Ingredients`.ingredientID = `RecipeIngredients`.ingredientID
+JOIN `Recipes` ON `Recipes`.recipeID = `RecipeIngredients`.recipeID
+GROUP BY recipeID;
+
+-- 2. Add ingredients to a recipe
+-- backend validation to ensure valid recipe and ingredient IDS
+INSERT INTO `RecipeIngredients` (recipeID, ingredientID, uOm, ingredientQuantity)
+VALUES (*recipeIDInput, *ingredientIDInput, *uOmInput, *ingredientQuantityInput);
--- recipe_runner_setup.sql
+++ recipe_runner_setup.sql
@@ -1,18 +1,23 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `ShoppingCarts`;
+DROP TABLE IF EXISTS `SelectedRecipes`;
+DROP TABLE IF EXISTS `Recipes`;
+DROP TABLE IF EXISTS `RecipeIngredients`;
+DROP TABLE IF EXISTS `Ingredients`;
+SET FOREIGN_KEY_CHECKS = 1;
+
 --
 -- Table structure for table `Users`
 --
 
-DROP TABLE IF EXISTS `Users`;
-
-CREATE TABLE `Users` (
+CREATE TABLE `Users`(
     `customerID` int(11) NOT NULL UNIQUE AUTO_INCREMENT,
-    `cart` int(11) UNIQUE NOT NULL,
     `fName` varchar(255) NOT NULL,
     `lName` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
     `zipCode` int(5) NOT NULL,
-    PRIMARY KEY (`customerID`),
-    CONSTRAINT `Users_fk_1` FOREIGN KEY (`cart`) REFERENCES `ShoppingCarts` (`cartID`),
+    PRIMARY KEY (`customerID`)
 );
 
 --
@@ -21,48 +26,79 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 INSERT INTO `Users` VALUES 
-    (1, 1, 'Timothy', 'Jan', 'jant@oregonstate.edu', 83854),
-    (2, 2, 'Benny', 'Li', 'libenn@oregonstate.edu', 99999),
-    (3, 3, 'Darth', 'Vader', 'vaderd@oregonstate.edu', 12345),
-    (4, 4, 'Luke', 'Skywalker', 'skywalkerl@oregonstate.edu', 54321),
-    (5, 5, 'Leia', 'Organa', 'organal@oregonstate.edu', 11111);
+    (1, 'Timothy', 'Jan', 'jant@oregonstate.edu', 83854),
+    (2, 'Benny', 'Li', 'libenn@oregonstate.edu', 99999),
+    (3, 'Darth', 'Vader', 'vaderd@oregonstate.edu', 12345),
+    (4, 'Luke', 'Skywalker', 'skywalkerl@oregonstate.edu', 54321),
+    (5, 'Leia', 'Organa', 'organal@oregonstate.edu', 11111);
 UNLOCK TABLES;
 
 --
 -- Table structure for table `ShoppingCarts`
 --
 
-DROP TABLE IF EXISTS `ShoppingCarts`;
-
-CREATE TABLE `ShoppingCarts` (
+CREATE TABLE `ShoppingCarts`(
     `cartID` int(11) UNIQUE NOT NULL AUTO_INCREMENT,
     `cartOwner` int(11) UNIQUE NOT NULL,
     PRIMARY KEY (`cartID`),
-    CONSTRAINT `ShoppingCarts_fk_1` FOREIGN KEY (`cartOwner`) REFERENCES `Users` (`customerID`),
+    CONSTRAINT `ShoppingCarts_fk_1` FOREIGN KEY (`cartOwner`) REFERENCES `Users` (`customerID`)
 );
 
 --
 -- Dumping data for table `ShoppingCarts`
--- Intersection table, no need to insert (?) 
--- Probably will need to test how this works
 --
 
 LOCK TABLES `ShoppingCarts` WRITE;
+INSERT INTO `ShoppingCarts` VALUES
+    -- timothy
+    (1, 1),
+    -- benny
+    (2, 2),
+    -- vader
+    (3, 3),
+    -- luke
+    (4, 4),
+    -- leia
+    (5, 5);
 UNLOCK TABLES;
+
+--
+-- Table structure for table `Recipes`
+--
+
+CREATE TABLE `Recipes`(
+    `recipeID` int(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    `recipeTitle` varchar(255) NOT NULL,
+    `recipeDescription` varchar(255) NOT NULL,
+    `recipeServing` int(11) NOT NULL,
+    PRIMARY KEY(`recipeID`)
+);
+
+-- 
+-- Dumping data for table `SelectedRecipes`
+--
+
+LOCK TABLES `Recipes` WRITE;
+INSERT INTO `Recipes` VALUES
+    (1, 'Chicken Tikka Masala', 'Chicken breast simmered in fragrant red Indian curry', 4),
+    (2, 'Beet Salad', 'Cubed boiled beets in balsamic dressing', 2),
+    (3, "Grandma's Apple Pie", 'A classic American dessert - tastes like the holidays', 8),
+    (4, 'Spicy Omelette', 'Southwestern style omelette with jalapenos and tomatoes', 2),
+    (5, 'BLT Sandwich', 'The perfect sandwich when you need to get rid of old stuff in your fridge', 1);
+UNLOCK TABLES;
+
 
 --
 -- Table structure for table `SelectedRecipes`
 --
 
-DROP TABLE IF EXISTS `SelectedRecipes`;
-
-CREATE TABLE `SelectedRecipes` (
+CREATE TABLE `SelectedRecipes`(
     `selectedCart` int(11) NOT NULL,
     `selectedRecipe` int(11) NOT NULL,
     `selectedQuantity` int(11) not NULL,
     PRIMARY KEY (`selectedCart`, `selectedRecipe`),
     CONSTRAINT `SelectedRecipes_fk_1` FOREIGN KEY (`selectedCart`) REFERENCES `ShoppingCarts` (`cartID`),
-    CONSTRAINT `SelectedRecipes_fk_2` FOREIGN KEY (`selectedRecipe`) REFERENCES `Recipes` (`recipeID`),
+    CONSTRAINT `SelectedRecipes_fk_2` FOREIGN KEY (`selectedRecipe`) REFERENCES `Recipes` (`recipeID`)
 );
 
 -- 
@@ -80,46 +116,52 @@ INSERT INTO `SelectedRecipes` VALUES
 UNLOCK TABLES;
 
 --
--- Table structure for table `Recipes`
+-- Table structure for table `Ingredients`
 --
 
-DROP TABLE IF EXISTS `Recipes`;
-
-CREATE TABLE `Recipes` (
-    `recipeID` int(11) NOT NULL UNIQUE AUTO_INCREMENT,
-    `recipeTitle` varchar(255) NOT NULL,
-    `recipeDescription` varchar(255) NOT NULL,
-    `recipeServing` int(11) NOT NULL,
-    PRIMARY KEY(`recipeID`),
+CREATE TABLE `Ingredients`(
+    `ingredientID` int(11) UNIQUE NOT NULL,
+    `ingredientName` varchar(255) NOT NULL,
+    `price` DECIMAL(6,2) NOT NULL,
+    PRIMARY KEY (`ingredientID`)
 );
 
 -- 
--- Dumping data for table `SelectedRecipes`
+-- Dumping data for table `Ingredients`
 --
 
-LOCK TABLES `Recipes` WRITE;
-INSERT INTO `Recipes` VALUES
-    (1, 'Chicken Tikka Masala', 'Chicken breast simmered in fragrant red Indian curry', 4),
-    (2, 'Beet Salad', 'Cubed boiled beets in balsamic dressing', 2),
-    (3, "Grandma's Apple Pie", 'A classic American dessert - tastes like the holidays', 8),
-    (4, 'Spicy Omelette', 'Southwestern style omelette with jalapenos and tomatoes', 2),
-    (5, 'BLT Sandwich', 'The perfect sandwich when you need to get rid of old stuff in your fridge', 1);
+LOCK TABLES `Ingredients` WRITE;
+INSERT INTO `Ingredients` VALUES
+    (1, 'Chicken Breast', 4.99),
+    (2, 'Tikka Masala Simmer Sauce', 6.99),
+    (3, 'Salt', 2.99),
+    (4, 'Black Pepper', 2.99),
+    (5, 'Beet', 3.99),
+    (6, 'Balsamic Dressing', 4.99),
+    (7, 'Apple (Granny Smith)', 3.99),
+    (8, 'Pie Crust', 6.99),
+    (9, 'Cinnamon', 7.99),
+    (10, 'Vanilla Ice Cream', 5.99),
+    (11, 'Egg', 0.50),
+    (12, 'Jalapeno', 0.99),
+    (13, 'Tomato', 0.99),
+    (14, 'Bacon', 3.99),
+    (15, 'Whole Wheat Bread', 4.99),
+    (16, 'Mayonnaise', 4.99);
 UNLOCK TABLES;
 
 --
 -- Table structure for table `RecipeIngredients`
 --
 
-DROP TABLE IF EXISTS `RecipeIngredients`;
-
 CREATE TABLE `RecipeIngredients`(
     `recipeID` int(11) NOT NULL,
     `ingredientID` int(11) NOT NULL,
     `uOm` varchar(255) NOT NULL,
     `ingredientQuantity` int(11) NOT NULL,
-    PRIMARY KEY (`recipeID`, `ingredientID`)
+    PRIMARY KEY (`recipeID`, `ingredientID`),
     CONSTRAINT `RecipeIngredients_fk_1` FOREIGN KEY (`recipeID`) REFERENCES `Recipes` (`recipeID`),
-    CONSTRAINT `RecipeIngredients_fk_2` FOREIGN KEY (`ingredientID`) REFERENCES `Ingredients` (`ingredientID`),
+    CONSTRAINT `RecipeIngredients_fk_2` FOREIGN KEY (`ingredientID`) REFERENCES `Ingredients` (`ingredientID`)
 );
 
 -- 
@@ -150,45 +192,8 @@ INSERT INTO `RecipeIngredients` VALUES
     (4, 12, 'japapeno', 4),
     (4, 13, 'tomato', 1), 
     -- BLT Sandwich (5)
-    (5, 12, 'tomato', 1), 
-    (5, 13, 'strip of bacon', 3), 
-    (5, 14, 'slice', 2), 
-    (5, 15, 'tbsp', 1); 
-UNLOCK TABLES;
-
---
--- Table structure for table `Ingredients`
---
-
-DROP TABLES IF EXISTS `Ingredients`;
-
-CREATE TABLE `Ingredients` (
-    `ingredientID` int(11) UNIQUE NOT NULL,
-    `ingredientName` varchar(255) NOT NULL,
-    `price` DECIMAL(6,2) NOT NULL,
-    PRIMARY KEY (`ingredientID`),
-);
-
--- 
--- Dumping data for table `Ingredients`
---
-
-LOCK TABLES `Ingredients` WRITE;
-INSERT INTO `Ingredients` VALUES
-    (1, 'Chicken Breast', 4.99),
-    (2, 'Tikka Masala Simmer Sauce', 6.99),
-    (3, 'Salt', 2.99),
-    (4, 'Black Pepper', 2.99),
-    (5, 'Beet', 3.99),
-    (6, 'Balsamic Dressing', 4.99),
-    (7, 'Apple (Granny Smith)', 3.99),
-    (8, 'Pie Crust', 6.99),
-    (9, 'Cinnamon', 7.99),
-    (10, 'Vanilla Ice Cream', 5.99),
-    (11, 'Egg', 0.50),
-    (12, 'Jalapeno', 0.99),
-    (12, 'Tomato', 0.99),
-    (13, 'Bacon', 3.99),
-    (14, 'Whole Wheat Bread', 4.99),
-    (15, 'Mayonnaise', 4.99);
+    (5, 13, 'tomato', 1), 
+    (5, 14, 'strip of bacon', 3), 
+    (5, 15, 'slice', 2), 
+    (5, 16, 'tbsp', 1); 
 UNLOCK TABLES;
