bd5f00707c484587daeb8061b89030f7d230b33f
Benny Li <libenn@flip3.engr.oregonstate.edu>
2021-11-18 14:28:35 -0800

    edit home page. remove .env tracking

--- recipe-runner/src/components/Home.js
+++ recipe-runner/src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
         <Grid container spacing={2} sx={{}}>
             <Grid item xs={4}><Card content='Browse and manage recipes' title="Recipes" href="/recipes"/></Grid>
             <Grid item xs={4}><Card content='Browse and manage ingredients' title="Ingredients" href="/ingredients"/></Grid>
-            <Grid item xs={4}><Card content='Browse and manage users on the app'title="Users" href="users"/></Grid>
+            <Grid item xs={4}><Card content='Browse and manage users'title="Users" href="users"/></Grid>
             <Grid item xs={4}><Card content='Browse and manage shopping carts' title="ShoppingCarts" href="shoppingcarts"/></Grid>
             <Grid item xs={4}><Card content='Browse selectedRecipes' title="SelectedRecipes" href="selectedrecipes"/></Grid>
             <Grid item xs={4}><Card content='Browse recipeIngredients' title="RecipeIngredients" href="recipeingredients"/></Grid>
