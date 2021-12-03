fb15f592ab578251925e19853d558cd9fb9c47f3
Timothy Jan <tj.jan@outlook.com>
2021-12-02 16:54:58 -0800

    changed website title, created new component for form dialogs with select interface

---
+++ recipe-runner/src/components/FormDialogSelect.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import FormInput from "./FormInput";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+
+export default function FormDialogSelect(props) {
+  const {buttonLabel, title, text, submitAction, inputs} = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleSubmit = () => {
+    setOpen(false);
+    submitAction();
+  }
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button variant="outlined" onClick={handleClickOpen} sx={{my:'auto', marginLeft: '1em'}}>
+        {buttonLabel}
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{text}</DialogContentText>
+          {inputs.map((input) => (
+            <FormInput id={input.id} label={input.label} type={input.type} key={input.key} hook={input.hook}/>
+          ))}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleSubmit}>Submit</Button>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
