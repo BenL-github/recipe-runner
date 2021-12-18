3c87c9ae1f6aeee7c6c7924ac3a10b219a81ff69
Benny Li <96bennyl@gmail.com>
2021-12-17 22:19:38 -0800

    test3

--- server.js
+++ server.js
@@ -13,22 +13,23 @@ const shoppingcartsRoutes = require('./routes/shoppingcarts')
 const selectedrecipesRoutes = require('./routes/selectedrecipes')
 const recipeingredientsRoutes = require('./routes/recipeingredients')
 const demo = require('./routes/demo')
-app.use(express.static(path.join(__dirname, "..", "build")))
+app.use(express.static(path.join(__dirname, "recipe-runner/build")))
 app.use(express.static("public"));
 app.use(cors())
 const port = process.env.PORT || 34876;
+
 
 // app.use((req, res, next) => {
 //     res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 //   });
 
-// app.use('/', ingredientsRoutes)
-// app.use('/', recipesRoutes)
-// app.use('/', usersRoutes)
-// app.use('/', shoppingcartsRoutes)
-// app.use('/', selectedrecipesRoutes)
-// app.use('/', recipeingredientsRoutes)
-// app.use('/', demo)
+app.use('/', ingredientsRoutes)
+app.use('/', recipesRoutes)
+app.use('/', usersRoutes)
+app.use('/', shoppingcartsRoutes)
+app.use('/', selectedrecipesRoutes)
+app.use('/', recipeingredientsRoutes)
+app.use('/', demo)
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/recipe-runner/build/index.html'));
